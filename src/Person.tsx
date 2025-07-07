dbc19fc3affdea8f0d9168895516ca5bfc4ae6cb
Makee <ina0312@naver.com>
2025-07-07 18:41:52 +0900

    1. Defining Props in TypeScript

---
+++ src/Person.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export interface Person {
+  name: string;
+  age: number;
+  isMarried: boolean;
+}
+
+export const Person = (props: Person) => {
+  const [isShowInfo, setShowInfo] = useState<boolean>(false);
+  return (
+    <div>
+      {isShowInfo && (
+        <>
+          <p>Name: {props.name}</p>
+          <p>Age: {props.age}</p>
+          <p>This person {props.isMarried ? "is married" : "is single"}</p>
+        </>
+      )}
+    </div>
+  );
+};
