2d4a6bd18692ebe5ba0640039933a11251e90509
Makee <ina0312@naver.com>
2025-07-07 21:49:07 +0900

    3. Enum in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
-import { User } from "./User";
+import { User, Countries } from "./User";
 import { UserProvider } from "./UserContextProvider";
 
 function App() {
   return (
     <UserProvider>
-      <User name={"Pedro"} age={34} isMarried={false} />
-      <User name={"Colin"} age={32} isMarried={true} />
+      <User
+        name={"Pedro"}
+        age={34}
+        isMarried={false}
+        country={Countries.Brazil}
+      />
+      <User
+        name={"Colin"}
+        age={32}
+        isMarried={true}
+        country={Countries.India}
+      />
     </UserProvider>
   );
 }
--- src/User.tsx
+++ src/User.tsx
@@ -1,20 +1,26 @@
-import { useContext } from "react";
-import { UserContext } from "./UserContextProvider";
-
 interface Props {
   name: string;
   age: number;
   isMarried: boolean;
+  country: Countries;
+}
+
+export enum Countries {
+  Brazil = "Brazil",
+  France = "France",
+  India = "India",
+  UnitedStates = "United States",
 }
 
 export const User = (props: Props) => {
-  const { user } = useContext(UserContext);
+  // const { users, updateUser } = useContext(UserContext);
 
   return (
     <div>
       <p>Name: {props.name}</p>
       <p>Age: {props.age}</p>
       <p>This person {props.isMarried ? "is married" : "is single"}</p>
+      <p>Country of Origin: {props.country}</p>
     </div>
   );
 };
